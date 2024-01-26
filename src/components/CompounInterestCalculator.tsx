342eed66f8d9e1c5f1c333e591a1a9756fbc1063
Memo Beltran <bego@codiks.com>
2024-01-26 02:53:00 -0600

    improve calculator

--- src/components/CompounInterestCalculator.tsx
+++ src/components/CompounInterestCalculator.tsx
@@ -3,11 +3,9 @@ import {
   Box,
   FormControl,
   FormLabel,
-  Button,
   NumberInput,
   NumberInputField,
   VStack,
-  Text,
   Table,
   Thead,
   Tbody,
@@ -15,13 +13,16 @@ import {
   Th,
   Td,
   Select,
-  Flex, // Added Select component
+  Flex,
+  InputGroup,
+  InputLeftAddon,
 } from "@chakra-ui/react";
 import {
   FaDollarSign,
   FaMoneyBillWave,
   FaChartLine,
   FaCoins,
+  FaCalendarAlt,
 } from "react-icons/fa"; // Added new icons
 import ResultCard from "./ResultCard";
 
@@ -53,10 +54,9 @@ const CompoundInterestCalculator = () => {
       total: number;
     }[] = [];
     let amount = principal;
-    let accumulatedDeposits = 0; // Added accumulatedDeposits variable
-    let accumulatedInterest = 0; // Added accumulatedInterest variable
+    let accumulatedDeposits = 0;
+    let accumulatedInterest = 0;
     for (let i = 0; i < years; i++) {
-      const yearPrincipal = i === 0 ? 0 : growthData[i - 1].total;
       const year = i + 1;
       const interest = amount * (rate / 100);
       accumulatedInterest += interest; // Updated accumulatedInterest
@@ -101,122 +101,163 @@ const CompoundInterestCalculator = () => {
 
   return (
     <Box p={4} borderWidth="1px" borderRadius="lg" overflow="hidden">
-      <VStack spacing={4}>
-        <FormControl id="principal">
-          <FormLabel>Inversión inicial</FormLabel>
-          <NumberInput
-            min={0}
-            onChange={(valueString) => setPrincipal(parseInt(valueString))}
-            value={format(principal.toString())}
-            onFocus={(e) => e.target.select()}
-          >
-            <NumberInputField placeholder="Inversión inicial" />
-          </NumberInput>
-        </FormControl>
-        <FormControl id="rate">
-          <FormLabel>Interés anual (%)</FormLabel>
-          <NumberInput
-            min={0}
-            max={100}
-            onChange={(valueString) => setRate(parseFloat(valueString))}
-          >
-            <NumberInputField placeholder="Interés anual" />
-          </NumberInput>
-        </FormControl>
-        <FormControl id="years">
-          <FormLabel>Plazo (Años)</FormLabel>
-          <NumberInput
-            min={0}
-            onChange={(valueString) => setYears(parseFloat(valueString))}
-          >
-            <NumberInputField placeholder="Plazo de crédito" value={years} />
-          </NumberInput>
-        </FormControl>
-        <FormControl id="frequency">
-          <FormLabel>Frecuencia de interés</FormLabel>
-          <Select
-            value={frequency}
-            onChange={(e) => setFrequency(e.target.value)}
-          >
-            <option value="annually">Anualmente</option>
-            <option value="monthly">Mensualmente</option>
-            <option value="weekly">Semanalmente</option>
-            <option value="daily">Diariamente</option>
-          </Select>
-        </FormControl>
-        <FormControl id="additionals">
-          <FormLabel>Aportes adicionales</FormLabel>
-          <NumberInput
-            min={0}
-            onChange={(valueString) => setAdditionals(parseFloat(valueString))}
-          >
-            <NumberInputField placeholder="Aportes adicionales" />
-          </NumberInput>
-        </FormControl>
-        {/* Removed Calculate button */}
-        {result && (
-          <VStack width="100%">
-            <Flex justifyContent="space-between" maxWidth={980} width="100%">
-              <ResultCard
-                label="Inversión inicial"
+      <Flex>
+        <VStack spacing={4} width="30%">
+          <FormControl id="principal">
+            <FormLabel>Inversión inicial</FormLabel>
+            <InputGroup>
+              <InputLeftAddon
+                pointerEvents="none"
+                color="gray.300"
+                fontSize="1.2em"
+                children={<FaDollarSign />}
+              />
+              <NumberInput
+                min={0}
+                onChange={(valueString) => setPrincipal(parseInt(valueString))}
                 value={format(principal.toString())}
-                icon={FaDollarSign}
+                onFocus={(e) => e.target.select()}
+                w="100%"
+              >
+                <NumberInputField
+                  placeholder="Inversión inicial"
+                  borderLeftRadius={0}
+                />
+              </NumberInput>
+            </InputGroup>
+          </FormControl>
+          <FormControl id="rate">
+            <FormLabel>Interés anual (%)</FormLabel>
+            <NumberInput
+              min={0}
+              max={100}
+              onChange={(valueString) => setRate(parseFloat(valueString))}
+              defaultValue={rate}
+            >
+              <NumberInputField placeholder="Interés anual" />
+            </NumberInput>
+          </FormControl>
+          <FormControl id="years">
+            <FormLabel>Plazo (Años)</FormLabel>
+            <NumberInput
+              min={0}
+              max={100}
+              onChange={(valueString) => {
+                const value = parseFloat(valueString);
+                setYears(value > 100 ? 100 : value);
+              }}
+              defaultValue={years}
+            >
+              <NumberInputField placeholder="Plazo de crédito" value={years} />
+            </NumberInput>
+          </FormControl>
+          <FormControl id="frequency">
+            <FormLabel>Frecuencia de interés</FormLabel>
+            <InputGroup>
+              <InputLeftAddon
+                pointerEvents="none"
+                color="gray.300"
+                fontSize="1.2em"
+                children={<FaCalendarAlt />}
               />
-              <ResultCard
-                label="Depósitos adicionales"
-                value={format(
-                  investmentGrowth[
-                    investmentGrowth.length - 1
-                  ]?.accumulatedDeposits?.toString() || "0"
-                )}
-                icon={FaMoneyBillWave} // Updated icon
+              <Select
+                value={frequency}
+                onChange={(e) => setFrequency(e.target.value)}
+                borderLeftRadius={0}
+              >
+                <option value="annually">Anualmente</option>
+                <option value="monthly">Mensualmente</option>
+                <option value="weekly">Semanalmente</option>
+                <option value="daily">Diariamente</option>
+              </Select>
+            </InputGroup>
+          </FormControl>
+          <FormControl id="additionals">
+            <FormLabel>Aportes adicionales</FormLabel>
+            <InputGroup>
+              <InputLeftAddon
+                pointerEvents="none"
+                color="gray.300"
+                fontSize="1.2em"
+                children={<FaDollarSign />}
               />
-              <ResultCard
-                label="Interés acumulado"
-                value={format(
-                  investmentGrowth[
-                    investmentGrowth.length - 1
-                  ]?.accumulatedInterest?.toString() || "0"
-                )}
-                icon={FaChartLine} // Updated icon
-              />
-              <ResultCard
-                label="Total"
-                value={format(
-                  investmentGrowth[
-                    investmentGrowth.length - 1
-                  ]?.total?.toString() || "0"
-                )}
-                icon={FaCoins} // Updated icon
-              />
-            </Flex>
-            <Flex overflowX="auto">
-              <Table variant="simple">
-                <Thead>
-                  <Tr>
-                    <Th>Año</Th>
-                    <Th>Depósito Inicial</Th>
-                    <Th>Depósitos Acumulados</Th>
-                    <Th>Interés Acumulado</Th>
-                    <Th>Total</Th>
+              <NumberInput
+                min={0}
+                onChange={(valueString) =>
+                  setAdditionals(parseFloat(valueString))
+                }
+                w="100%"
+              >
+                <NumberInputField
+                  placeholder="Aportes adicionales"
+                  borderLeftRadius={0}
+                />
+              </NumberInput>
+            </InputGroup>
+          </FormControl>
+        </VStack>
+        {/* Removed Calculate button */}
+        <VStack width="70%">
+          <Flex justifyContent="space-between" maxWidth={980} width="100%">
+            <ResultCard
+              label="Inversión inicial"
+              value={format(principal.toString())}
+              icon={FaDollarSign}
+            />
+            <ResultCard
+              label="Depósitos adicionales"
+              value={format(
+                investmentGrowth[
+                  investmentGrowth.length - 1
+                ]?.accumulatedDeposits?.toString() || "0"
+              )}
+              icon={FaMoneyBillWave} // Updated icon
+            />
+            <ResultCard
+              label="Interés acumulado"
+              value={format(
+                investmentGrowth[
+                  investmentGrowth.length - 1
+                ]?.accumulatedInterest?.toString() || "0"
+              )}
+              icon={FaChartLine} // Updated icon
+            />
+            <ResultCard
+              label="Total"
+              value={format(
+                investmentGrowth[
+                  investmentGrowth.length - 1
+                ]?.total?.toString() || "0"
+              )}
+              icon={FaCoins} // Updated icon
+            />
+          </Flex>
+          <Flex overflowX="auto">
+            <Table variant="simple">
+              <Thead>
+                <Tr>
+                  <Th>Año</Th>
+                  <Th>Depósito Inicial</Th>
+                  <Th>Depósitos Acumulados</Th>
+                  <Th>Interés Acumulado</Th>
+                  <Th>Total</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {investmentGrowth.map((data) => (
+                  <Tr key={data.year}>
+                    <Td>{data.year}</Td>
+                    <Td>${format(data.initDeposit.toString())}</Td>
+                    <Td>${format(data.accumulatedDeposits.toString())}</Td>
+                    <Td>${format(data.accumulatedInterest.toString())}</Td>
+                    <Td>${format(data.total.toString())}</Td>
                   </Tr>
-                </Thead>
-                <Tbody>
-                  {investmentGrowth.map((data) => (
-                    <Tr key={data.year}>
-                      <Td>{data.year}</Td>
-                      <Td>${format(data.initDeposit.toString())}</Td>
-                      <Td>${format(data.accumulatedDeposits.toString())}</Td>
-                      <Td>${format(data.accumulatedInterest.toString())}</Td>
-                      <Td>${format(data.total.toString())}</Td>
-                    </Tr>
-                  ))}
-                </Tbody>
-              </Table>
-            </Flex>
-          </VStack>
-        )}
-      </VStack>
+                ))}
+              </Tbody>
+            </Table>
+          </Flex>
+        </VStack>
+      </Flex>
     </Box>
   );
 };
